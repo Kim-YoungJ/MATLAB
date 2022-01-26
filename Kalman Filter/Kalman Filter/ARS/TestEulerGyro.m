57f9905db85e1f93a1c888051fbef59e391bb49d
K.YoungJ <k.youngj2528@gmail.com>
2022-01-26 12:37:24 +0900

    first commit

---
+++ Kalman Filter/Kalman Filter/ARS/TestEulerGyro.m
@@ -0,0 +1,37 @@
+clear all
+clc
+close all
+
+Nsamples = 41500;
+EulerSaved = zeros(Nsamples, 3);
+
+dt = 0.01;
+
+for k = 1:Nsamples
+  [p q r] = GetGyro();   
+  [phi theta psi] = EulerGyro(p, q, r, dt); 
+  
+  EulerSaved(k, :) = [ phi theta psi ];
+end 
+
+
+PhiSaved   = EulerSaved(:, 1) * 180/pi;
+ThetaSaved = EulerSaved(:, 2) * 180/pi;
+PsiSaved   = EulerSaved(:, 3) * 180/pi;
+
+t = 0:dt:Nsamples*dt-dt;
+
+figure
+plot(t, PhiSaved)
+xlabel('Time[sec]')
+ylabel('Roll angle[deg]')
+
+figure
+plot(t, ThetaSaved)
+xlabel('Time[sec]')
+ylabel('Pitch angle[deg]')
+
+figure
+plot(t, PsiSaved)
+xlabel('Time[sec]')
+ylabel('Yaw angle[deg]')
